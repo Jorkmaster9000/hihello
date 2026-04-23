75cf84512fa83a85f068464976aa3270a59cb18b
Jorkmaster9000 <idclemmeuse@gmail.com>
2026-04-23 20:47:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -420,5 +420,27 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     slider.appendChild(card);
+
+          (function (w, d, s, o, f, js, fjs) {
+        w["botsonic_widget"] = o;
+        w[o] =
+          w[o] ||
+          function () {
+            (w[o].q = w[o].q || []).push(arguments);
+          };
+        (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+        js.id = o;
+        js.src = f;
+        js.async = 1;
+        fjs.parentNode.insertBefore(js, fjs);
+      })(window, document, "script", "Botsonic", "https://widget.botsonic.com/CDN/botsonic.min.js");
+      Botsonic("init", {
+        serviceBaseUrl: "https://api-bot.writesonic.com",
+        token: "f8b66dff-b6c9-4188-8545-78e9f651bcbf",
+      });
+
+
+
+    
   }
 });
